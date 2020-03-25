6a2280c970b75d0e17720c6612d4fe46590c3ec4
ChrisBLong <57813875+ChrisBLong@users.noreply.github.com>
2020-03-25 16:49:38 +0000

    Added GPLv3 licence and corresponding headers etc.

--- js/point.js
+++ js/point.js
@@ -1,5 +1,24 @@
+/*
+    Formula One Inverted Simulator.
+    Copyright (C) 2020 Chris Long of Oceanview Consultancy Ltd.
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 // Define a class to represent a 2D point.
 // It's easier to pass a Point around than to keep passing separate X and Y coordinates.
+
 export class Point {
 	
   constructor(x, y) {
